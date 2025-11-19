cb0a77b5addfab079d5e6a9b274a6a864d7f57df
ekae5355-collab <ekae5355@gmail.com>
2025-11-19 16:55:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<head>
+<style>
+body {
+background: linear-gradient(lightblue, white );
+color: grey ; 
+text-align: center; }
+h1 { ... }
+i {
+font-size: 25px;
+}
+</style>
+</head>
+<body>
+<img src="/sprites/moon.png " >
+<h1>   
+Bulan
+</h1> 
+<h2>
+Motivation: Berjuanglah Sampai Melampaui Awan
+<i>
+. Keep going beyond the clouds lies your victory
+</i>
+</h2>
+<h3>
+Launching Soon....
+</h3>
+<footer>
+&copy; 2025 Ningrum
+</footer>
+</body>
+<style>
+...
+h2{ font-size: 30px; 
+border: 2px solid white;
+padding: 10%; 
+margin: 12%; }
+</style>
+<style>
+...
+footer {
+background-size: 10px 10px;
+background-image: radial-gradient(purple 1px, black 1px);
+background-color: black;
+padding: 15px;
+background-size: 10px 10px;
+}
+</style>
+
